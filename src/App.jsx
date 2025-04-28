c32d72a55fda70c7da9889ab3fd82eeae76811f4
R-1493 <retajharbi319@gmail.com>
2025-04-28 12:15:06 +0300

    Fix: updated App.jsx import and project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import "./App.css";
-import Main from "./Components/main";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import Main from "./Components/Main";
 function App() {
   const url = "https://dummyjson.com/products";
   return (
